b04c15dcb70cbadc121bf205740abaa5fd8a9011
Milos Radosavljevic <milosrcd@gmail.com>
2022-11-16 07:25:17 +0100

    nov16

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,48 +1,37 @@
-const errorMessages = document.getElementsByClassName("errorMessage");
+const errorMessages = document.getElementsByClassName("errorMessage"); 
 
-for(let message of errorMessages){
+for(let message of errorMessages) {
     message.classList.add("display-none");
 }
 
-document.getElementById("programSubmit").addEventListener("click", function(){
-    const form = document.getElementById("inputForm");
-    form.submit();
-});
-
-document.getElementById("inputForm").addEventListener("submit", function(event){
+document.getElementById("inputForm").addEventListener("submit", function(event) {
     event.preventDefault();
     let isFormValid = true;
-    const email = document.getElementById("email");
-    const regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    const result = email.value.match(regEmail);
-    console.log(result);
+    const email = document.getElementById("email"); 
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+    const isEmailValid = regexEmail.test(email.value);
 
-    const isEmailValid = result === null;
-    toggleErrorMessage(isEmailValid, email);
+    toggleErrorMessage(!isEmailValid, email);
 
-    if(isEmailValid){
+    if(!isEmailValid) {
         isFormValid = false;
     }
 
     const password = document.getElementById("password");
-    //To check a password between 7 to 16 characters which contain only characters, numeric digits, underscore and first character must be a letter
-    const regPassword = /^[A-Za-z]\w{7,14}$/;
+    const regexPass = /([A-Za-z]){8,}\w+/ ; 
 
-    const resultPass = password.value.match(regPassword);
-    console.log(resultPass);
-    
-    const isPassValid = result === null;
-    toggleErrorMessage(isPassValid, password);
-    if(isPassValid){
+    const isPasslValid = regexPass.test(password.value);
+
+    toggleErrorMessage(!isPasslValid, password);
+
+    if(!isPasslValid) {
         isFormValid = false;
     }
 
-    if(isFormValid){
+    if(isFormValid) {
         event.target.submit();
     }
-
-
 });
 
 function toggleErrorMessage(isFieldNotValid, formElement) {
@@ -52,3 +41,4 @@ function toggleErrorMessage(isFieldNotValid, formElement) {
         formElement.parentElement.querySelector(".errorMessage").classList.add("display-none");
     }
 }
+
